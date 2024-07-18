3c7e7c04f3a777c6c5236419df55eb638502a941
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-18 22:17:36 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+}
+
+OUTPUT:
+
+*****
+*****
+*****
+*****
+*****
