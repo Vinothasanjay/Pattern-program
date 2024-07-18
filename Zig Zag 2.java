fe44794182803e4ee33192b1f36511a2068c72b2
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-18 22:08:11 +0530

    Create Zig Zag 2.java

---
+++ Zig Zag 2.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+         if(j==(n/2)+1 ||(i==1 && j<(n/2)+1) ||(i==n&& j>(n/2)+1)){
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+OUTPUT:
+* * *     
+    *     
+    *     
+    *     
+    * * * 
