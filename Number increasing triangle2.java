4a3e6956c27dd938dc456602148e9dddbc4c9398
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-19 08:26:18 +0530

    Create Number increasing triangle2.java

---
+++ Number increasing triangle2.java
@@ -0,0 +1,24 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Try programiz.pro");
+        int n=5;
+        int k=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(k+" ");
+              
+                
+            }
+            k=k+1;
+          
+            System.out.println();
+        }
+    }
+}
+OUTPUT:
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
