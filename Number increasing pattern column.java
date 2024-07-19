b03252a0ddd25264a2dc42a96a40de01762f9643
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-19 14:35:04 +0530

    Create Number increasing pattern column.java

---
+++ Number increasing pattern column.java
@@ -0,0 +1,18 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Try programiz.pro");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+OUTPUT:
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
