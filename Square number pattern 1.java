6ab7f66ce66aa56e8168126df3f2e01af5c91884
VINOTHA S <113914407+Vinothasanjay@users.noreply.github.com>
2024-07-19 08:21:00 +0530

    Create Square number pattern 1.java

---
+++ Square number pattern 1.java
@@ -0,0 +1,18 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Try programiz.pro");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+OUTPUT:
+1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 3 
+4 4 4 4 4 
+5 5 5 5 5
